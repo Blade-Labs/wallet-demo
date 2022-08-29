b5c4266b99945683bdf11a810a602fca54e08b78
Igor Ognichenko <ognichenko.igor@gmail.com>
2022-08-29 16:12:12 +0300

    WWT-1173: improvement demo wallet for nfts

---
+++ src/components/forms/FormAssociateToken.vue
@@ -0,0 +1,36 @@
+<script setup lang='ts'>
+import { AccountId } from "@hashgraph/sdk";
+import { useBladeStore } from '../../store/blade-signer';
+import { toHexBytes } from '../../utils/encode';
+
+const bladeStore = useBladeStore();
+const tokenId = ref<string>("0.0.47717806");
+
+const onSubmit = async ()=>{
+  const result = await bladeStore.tokenAssociate({
+    recipient: bladeStore.accountId as AccountId,
+    tokenId: tokenId.value as string,
+  });
+  const receipt = await bladeStore.getTransactionReceipt(result!.transactionId);
+
+  return `Transaction id: ${result?.transactionId}\n`+
+    `Transaction status: ${receipt.status}\n`+
+    `Receipt bytes: ${ toHexBytes( receipt.toBytes() )}`
+  ;
+}
+
+const canSubmit = computed(()=>{
+  return Boolean(tokenId?.value);
+});
+
+</script>
+
+<template>
+  <vue-form
+    title="Associate Token"
+    :onSubmit="onSubmit"
+    :canSubmit="canSubmit"
+  >
+    <text-box label="Token ID" v-model="tokenId" />
+  </vue-form>
+</template>
--- src/store/blade-signer.ts
+++ src/store/blade-signer.ts
@@ -1,20 +1,24 @@
-import type {
+import {
   AccountId,
   Transaction,
   Signer,
   Executable,
+  TokenId,
+  TokenAssociateTransaction,
 } from "@hashgraph/sdk";
 import {
   TransactionReceiptQuery,
   TransferTransaction,
   TransactionId,
   AccountInfoQuery,
+  Client,
 } from "@hashgraph/sdk";
 
 import { defineStore } from "pinia";
 import BigNumber from "bignumber.js";
 import { useBalanceStore } from "./balance-store";
 import { BladeSigner } from "@bladelabs/blade-web3.js";
+import Long from "long";
 
 type BladeStoreState = {
   signer: Signer | null;
@@ -95,6 +99,93 @@ export const useBladeStore = defineStore("blade-store", {
             amount: transfer.amount.negated(),
           },
         ],
+      });
+
+      const result = await this.signer.call(transaction);
+
+      this.fetchMyBalance();
+
+      return result;
+    },
+
+    async nftTransfer(transfer: {
+      tokenId: TokenId | string,
+      sender: AccountId | string,
+      recipient: AccountId | string,
+      serial: Long | number
+    }) {
+      if (this.signer == null) {
+        return;
+      }
+
+      const transaction = new TransferTransaction({
+        nftTransfers: [
+          {
+            tokenId: transfer.tokenId,
+            sender: transfer.sender,
+            recipient: transfer.recipient,
+            serial: transfer.serial
+          },
+        ],
+      });
+
+      const result = await this.signer.call(transaction);
+
+      this.fetchMyBalance();
+
+      return result;
+    },
+
+    async nftBuy(transfer: {
+      tokenId: TokenId | string,
+      accountFrom: AccountId | string,
+      serial: Long | number,
+      amount: BigNumber,
+    }) {
+      if (this.signer == null) {
+        return;
+      }
+
+      const currentAccount = this.accountId! as AccountId;
+      const transaction = new TransferTransaction({
+        nftTransfers: [
+          {
+            tokenId: transfer.tokenId,
+            sender: transfer.accountFrom,
+            recipient: currentAccount,
+            serial: transfer.serial
+          },
+        ],
+        hbarTransfers: [
+          {
+            accountId: currentAccount,
+            amount: transfer.amount.negated(),
+          },
+          {
+            accountId: transfer.accountFrom,
+            amount: transfer.amount,
+          },
+        ],
+      });
+
+      const result = await this.signer.call(transaction);
+
+      this.fetchMyBalance();
+
+      return result;
+    },
+
+    async tokenAssociate(params: {
+      tokenId: TokenId | string,
+      recipient: AccountId | string,
+    }) {
+      if (this.signer == null) {
+        return;
+      }
+      
+      const transaction = new TokenAssociateTransaction({
+        tokenIds: [params.tokenId],
+        accountId: params.recipient,
       });
 
       const result = await this.signer.call(transaction);
