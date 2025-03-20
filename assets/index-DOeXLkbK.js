8403b9083249a723a00809e28e31b267d16b188e
Volind <rozhnov.vladimir@gmail.com>
2025-03-20 08:10:41 +0000

    Deploying to wallet-demo from @ Blade-Labs/wallet-demo@e82b51d9ade79820c9e6be27b132bb3fbfff095e 🚀

--- assets/index-DOeXLkbK.js
+++ assets/index-DOeXLkbK.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as U,a as E,o as ot,R as Do,p as q,y as gt}from"./index-CSquA_I3.js";/**
+import{n as _t,s as ce,T as N,t as U,a as E,o as ot,R as Do,p as q,y as gt}from"./index-WFXXHLnJ.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
